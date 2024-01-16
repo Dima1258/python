072affd964c48663230cc93a3d2fcb9d414e52d3
Dima1258 <84058579+Dima1258@users.noreply.github.com>
2024-01-16 22:46:48 +0900

    Add files via upload

---
+++ bot/maine.py
@@ -0,0 +1,36 @@
+import random
+import discord
+from discord.ext import commands
+
+# Переменная intents - хранит привилегии бота
+intents = discord.Intents.default()
+# Включаем привелегию на чтение сообщений
+intents.message_content = True
+# Создаем бота в переменной client и передаем все привелегии
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+bot.run("MTE4OTk0MDE3MzUzMDczMDYzOA.Gv12l2.cyn-mW5klzrM-W1uFZ0toPs6ZQJOfKUON9lWG8")
